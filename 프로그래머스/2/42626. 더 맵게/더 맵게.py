4ffe24978443890be824281b27ed35b442a753f4
Minkyo <141614581+MinkyoJeong1@users.noreply.github.com>
2024-02-06 20:59:38 +0900

    [level 2] Title: 더 맵게, Time: 1675.81 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    cnt = 0
+    
+    while scoville[0] < K:
+        mix = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, mix)
+        
+        cnt += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    return cnt
