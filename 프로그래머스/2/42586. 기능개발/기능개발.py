0819e5bf0a21289589a7b836e0baa5578d725ba5
Minkyo <141614581+MinkyoDev@users.noreply.github.com>
2024-04-27 20:47:45 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,9 @@
-from collections import deque
-from math import ceil
-
 def solution(progresses, speeds):
-    complestion_day = deque([ceil((100-progress)/speed) for progress, speed in zip(progresses, speeds)])
-    
-    temp = 0
-    temp_list = []
-    answer = []
-    for day in complestion_day:
-        if day > temp:
-            answer.append(len(temp_list))
-            temp_list = []
-            temp = day
-            temp_list.append(day)
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
         else:
-            temp_list.append(day)
-    else:
-        answer.append(len(temp_list))
-    return answer[1:]
+            Q[-1][1]+=1
+        print(Q)
+    return [q[1] for q in Q]
