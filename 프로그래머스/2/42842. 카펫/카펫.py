2c29f1b472a8a903fa846cdee904884290fd5670
Minkyo <minkyo.dev@gmail.com>
2024-10-16 11:15:28 +0900

    [level 2] Title: 카펫, Time: 157.64 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,6 +1,7 @@
 def solution(brown, yellow):
-    for i in range(1, yellow+1):
-        for j in range(1,i+1):
-            if i*j == yellow:
-                if (i+2)*(j+2) == (brown+yellow):
-                    return [i+2, j+2]
+    total = brown + yellow
+    for i in range(3, total):
+        for j in range(3, i+1):
+            if i * j == total:
+                if 2 * (i + j) - 4 == brown:
+                    return [i, j]
